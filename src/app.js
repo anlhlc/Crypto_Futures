8552ea32d9e45f34662fc12a06193c38164d7347
anlhlc <83272690+anlhlc@users.noreply.github.com>
2026-02-02 16:18:44 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+const authRoutes = require('./routes/auth.routes');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'tradepulse_secret',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../views')));
+
+app.use('/', authRoutes);
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, '../views/404.html'));
+});
+
+module.exports = app;
 
